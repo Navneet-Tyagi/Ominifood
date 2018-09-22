82d4785c2e0a50ee2dbbd786c4d4b032ebbc32ac
Navneet Tyagi <SESA408873@schneider-electric.com>
2018-09-22 12:34:50 +0530

    ok

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  
+  /* For sticky Navigation*/
+  $('.js--section-features').waypoint(function(direction){
+    if(direction== "down"){
+$('nav').addClass('sticky');
+    }
+    else
+    {
+$('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+  
+  /*Scroll on buttons */
+
+  $('.js--scroll-to-plans').click(function(){
+$('html, body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
+  });
+
+  /*Animation on scroll */
+  
+});
